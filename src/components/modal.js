0f205538bf9ca430ccf386a93eeee355e49dfe62
MaxouGit <MaxouGit@maxenceleroux.fr>
2023-12-31 13:15:25 +0100

    Add react-icons and react-rnd dependencies

---
+++ src/components/modal.js
@@ -0,0 +1,116 @@
+// components/Modal.js
+import React, { useState, useEffect } from "react";
+import { Rnd } from "react-rnd";
+import "../styles/modal.css";
+
+let highestZIndex = 1000;
+
+const Modal = ({ children, closeModal }) => {
+  const [isMaximized, setIsMaximized] = useState(false);
+  const [dimensions, setDimensions] = useState({
+    width: 650,
+    height: 200,
+    x: 50,
+    y: 10,
+  });
+  const [lastDimensions, setLastDimensions] = useState({ ...dimensions });
+  const [zIndex, setZIndex] = useState(highestZIndex);
+
+  useEffect(() => {
+    function handleResize() {
+      if (isMaximized) {
+        setDimensions({
+          width: window.innerWidth,
+          height: window.innerHeight,
+          x: 0,
+          y: 0,
+        });
+      }
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [isMaximized]);
+
+  const bringToFront = () => {
+    highestZIndex += 1;
+    setZIndex(highestZIndex);
+  };
+
+  const modalStyle = {
+    zIndex: zIndex,
+    width: isMaximized ? window.innerWidth : dimensions.width,
+    height: isMaximized ? window.innerHeight : dimensions.height,
+  };
+
+  const handleDragStart = () => {
+    if (isMaximized) {
+      // Réduit la fenêtre lorsqu'elle est déplacée en mode plein écran
+      setIsMaximized(false);
+      setDimensions(lastDimensions);
+    }
+  };
+
+  const toggleMaximize = () => {
+    if (isMaximized) {
+      setDimensions(lastDimensions);
+    } else {
+      setLastDimensions(dimensions);
+      // Utilisez clientWidth et clientHeight pour obtenir la largeur et la hauteur intérieures
+      setDimensions({
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight,
+        x: 0,
+        y: 0,
+      });
+    }
+    setIsMaximized(!isMaximized);
+  };
+
+  return (
+    <Rnd
+      size={{ width: dimensions.width, height: dimensions.height }}
+      position={{ x: dimensions.x, y: dimensions.y }}
+      style={modalStyle}
+      onMouseDown={bringToFront}
+      onDragStart={handleDragStart}
+      onDragStop={(e, d) => {
+        if (!isMaximized) {
+          setDimensions({ ...dimensions, x: d.x, y: d.y });
+        }
+      }}
+      onResizeStop={(e, direction, ref, delta, position) => {
+        if (!isMaximized) {
+          setDimensions({
+            width: ref.offsetWidth,
+            height: ref.offsetHeight,
+            ...position,
+          });
+        }
+      }}
+      minWidth={400}
+      minHeight={200}
+      maxWidth={window.innerWidth}
+      maxHeight={window.innerHeight}
+      bounds="window"
+      enableResizing={!isMaximized}
+      dragHandleClassName="modal-header"
+    >
+      <div className="modal-content">
+        <div className="modal-header" onDoubleClick={toggleMaximize}>
+          <div className="window-controls">
+            {/* Bouton de maximisation/restauration */}
+            <button onClick={toggleMaximize} className="window-control-button">
+              {isMaximized ? "🗗" : "🗖"}
+            </button>
+            <button onClick={closeModal} className="window-control-button">
+              &times;
+            </button>
+          </div>
+        </div>
+        <div className="modal-body">{children}</div>
+      </div>
+    </Rnd>
+  );
+};
+
+export default Modal;
